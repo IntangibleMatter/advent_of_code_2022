b96869a4cdc5975b62a3968ac23c90761ecebae3
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2022-12-02 12:07:44 -0800

    day 2 final

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -3,15 +3,15 @@ input_values = open("day_2\input.txt")
 game_sets = []
 
 rps = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3
+    "r": 1,
+    "p": 2,
+    "s": 3
 }
 
 lwd = {
-    "l": 0,
-    "w": 6,
-    "d": 3
+    "X": 0,
+    "Y": 3,
+    "Z": 6
 }
 
 score = 0
@@ -23,38 +23,38 @@ input_values.close()
 
 for game in game_sets:
     match game[0]:
-        case "A":
+        case "A": # rock
+            match game[2]:
+                case "X": # loss
+                    score += lwd["X"]
+                    score += rps["s"]
+                case "Y": # draw
+                    score += lwd["Y"]
+                    score += rps["r"]
+                case "Z": #win
+                    score += lwd["Z"]
+                    score += rps["p"]
+        case "B": # paper
             match game[2]:
                 case "X":
-                    score += rps["X"]# rock
-                    score += lwd["d"]
+                    score += lwd["X"]
+                    score += rps["r"]
                 case "Y":
-                    score += rps["Y"]
-                    score += lwd["w"]
+                    score += lwd["Y"]
+                    score += rps["p"]
                 case "Z":
-                    score += rps["Z"]
-                    score += lwd["l"]
-        case "B":
+                    score += lwd["Z"]
+                    score += rps["s"]
+        case "C": # scissors
             match game[2]:
                 case "X":
-                    score += rps["X"]# rock
-                    score += lwd["l"]
+                    score += lwd["X"]
+                    score += rps["p"]
                 case "Y":
-                    score += rps["Y"]
-                    score += lwd["d"]
+                    score += lwd["Y"]
+                    score += rps["s"]
                 case "Z":
-                    score += rps["Z"]
-                    score += lwd["w"]
-        case "C":
-            match game[2]:
-                case "X":
-                    score += rps["X"]# rock
-                    score += lwd["w"]
-                case "Y":
-                    score += rps["Y"]
-                    score += lwd["l"]
-                case "Z":
-                    score += rps["Z"]
-                    score += lwd["d"]
+                    score += lwd["Z"]
+                    score += rps["r"]
 
 print(score)
